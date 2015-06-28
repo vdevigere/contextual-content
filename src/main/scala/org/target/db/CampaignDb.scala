65b629717de24a812fed240e3a90d98877e9618f
vdevigere <vdevigere+git@gmail.com>
2015-06-27 22:08:40 -0700

    Moving the redis cache store to separate module
    Switching to use the Scala version of infinispan-cachestore-redis

--- src/main/scala/org/target/db/CampaignDb.scala
+++ src/main/scala/org/target/db/CampaignDb.scala
@@ -1,18 +1,21 @@
 package org.target.db
 
-import org.infinispan.Cache
-import org.infinispan.configuration.cache.{Configuration, ConfigurationBuilder}
-import org.infinispan.eviction.EvictionStrategy._
-import org.infinispan.manager.{DefaultCacheManager, EmbeddedCacheManager}
+import java.net.URI
+
+import com.viddu.infinispan.redis.configuration.RedisStoreConfigurationBuilder
+import org.infinispan.configuration.cache.ConfigurationBuilder
+import org.infinispan.manager.DefaultCacheManager
 import org.target.Campaign
-import collection.JavaConversions._
+
+import scala.collection.JavaConversions._
 
 /**
  * Created by Viddu on 6/13/2015.
  */
 object CampaignDb {
   val manager = new DefaultCacheManager()
-  val configuration = new ConfigurationBuilder().persistence().passivation(false).addStore(classOf[RedisStoreConfigurationBuilder]).host("localhost").port(6379).build()
+  val configuration = new ConfigurationBuilder().persistence().passivation(false).addStore(classOf[RedisStoreConfigurationBuilder])
+    .url(new URI("redis://localhost:6379/0")).build()
   manager.defineConfiguration("campaign-cache", configuration)
   private val cache = manager.getCache[Long, Campaign]("campaign-cache")
 
